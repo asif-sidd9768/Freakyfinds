1a65bd04f0265d6689eab808c55eb79de5f55872
asif-sidd9768 <siddiqueasif421@gmail.com>
2023-05-30 15:31:19 +0530

    added featured product on home page

--- src/components/HomePage/FeaturedProducts/FeaturedProducts.js
+++ src/components/HomePage/FeaturedProducts/FeaturedProducts.js
@@ -6,13 +6,14 @@ import "./FeaturedProducts.css"
 
 export const FeaturedProducts = () => {
   const { productState } = useContext(ProductContext)
+  const featuredProducts = productState.products.filter(({isFeatured}) => isFeatured).slice(0,4)
   return (
     <div>
       <p className="featured-products-title">Featured Products</p>
       <p className="featured-products-tagline">Power up with our featured products!</p>
       <div className="featured-products-container">
         {
-          productState.products.slice(0,4).map(pr => 
+          featuredProducts.map(pr => 
             <FeaturedProductCard key={pr.id} {...pr} />
           )
         }
